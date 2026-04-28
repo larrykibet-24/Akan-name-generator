c6e385c22c5b4eb74aa1ff4fbd79fcb22d0a07c9
Larry <larry.kibet@gmail.com>
2026-04-28 08:48:03 +0300

    Add comment to clarify purpose of male names array

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//array of male Akan names
 const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame",];
 
 const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
