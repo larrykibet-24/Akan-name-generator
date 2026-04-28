67a714d4d4fc44b3a40d7c850be76fc5d3129d8a
Larry <larry.kibet@gmail.com>
2026-04-28 10:32:27 +0300

    Add male and female names arrays and implement submit button event listener

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const submitButton = document.querySelector("#submit-button")
+const maleNames = [ "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+const submitButton = document.querySelector("#submit-button");
 let CC;
 let YY;
 let MM;
 let DD;
 let d;
 
-submitButton.addEventListener
+submitButton.addEventListener("click", (event) =>){
+    //prevent form from refreshing page
+    event.preventDefault();
+
+    //retrieve values from the form
+    CC = 
+    YY = 
+    MM = document.querySelector("month").value;
+    DD = document.querySelector("day").value;
+
+}
