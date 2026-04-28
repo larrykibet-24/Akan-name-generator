a3809b5393149f129f1efd0d4db8e4db0d9aa852
Larry <larry.kibet@gmail.com>
2026-04-28 10:56:30 +0300

    Fix event listener for submit button and correct value retrieval from form fields

--- script.js
+++ script.js
@@ -8,14 +8,22 @@ let MM;
 let DD;
 let d;
 
-submitButton.addEventListener("click", (event) =>){
+submitButton.addEventListener("click", (event) =>{
     //prevent form from refreshing page
     event.preventDefault();
 
     //retrieve values from the form
-    CC = document.querySelector(Math.floor("year" / 100))
-    YY =year % 100 
-    MM = document.querySelector("month").value;
-    DD = document.querySelector("day").value;
+    CC = document.querySelector(Math.floor("#year" / 100))
+    YY =document.querySelector("#year" % 100) 
+    MM = document.querySelector("#month").value;
+    DD = document.querySelector("#day").value;
 
-}
+    const dayIndex = Math.floor(
+    (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7,
+  );
+   const resultDiv = document.getElementById("result");
+  const displayP = document.getElementById("display-name");
+
+  displayP.textContent = akanName;
+  resultDiv.classList.remove(".hidden");
+});
